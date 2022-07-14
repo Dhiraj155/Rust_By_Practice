528f55ad14337d031c6f748ac07f82ce7fbfaa8a
Dhiraj155 <dhirajwani155@gmail.com>
2022-07-14 21:01:08 +0530

    Que 5

---
+++ Basic_Types/Functions/Diverging_functions_Que5.rs
@@ -0,0 +1,34 @@
+
+fn main() {
+    // FILL in the blank
+    let b = __;
+
+    let v = match b {
+        true => 1,
+        // Diverging functions can also be used in match expression to replace a value of any value
+        false => {
+            println!("Success!");
+            panic!("we have no value for `false`, but we can panic");
+        }
+    };
+
+    println!("Exercise Failed if printing out this line!");
+}
+
+//Ans----
+
+fn main() {
+    // FILL in the blank
+    let b = false;
+
+    let v = match b {
+        true => 1,
+        // Diverging functions can also be used in match expression
+        false => {
+            println!("Success!");
+            panic!("we have no value for `false`, but we can panic")
+        }
+    };
+
+    println!("Excercise Failed if printing out this line!");
+}
