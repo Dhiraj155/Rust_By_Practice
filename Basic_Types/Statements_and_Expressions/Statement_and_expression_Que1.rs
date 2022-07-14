02a3dc52e1333137a7bd9277bf674d612181faf4
Dhiraj155 <dhirajwani155@gmail.com>
2022-07-14 20:41:19 +0530

    Que1

---
+++ Basic_Types/Statements_and_Expressions/Statement_and_expression_Que1.rs
@@ -0,0 +1,22 @@
+// Make it work with two ways
+fn main() {
+    let v = {
+        let mut x = 1;
+        x += 2
+    };
+ 
+    assert_eq!(v, 3);
+ 
+    println!("Success!");
+ }
+
+ //Ans---
+
+ fn main() {
+    let v = {
+        let mut x = 1;
+        x += 2
+    };
+ 
+    assert_eq!(v, ());
+ }
