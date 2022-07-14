6a540fd6ddcf9a38dfe053e1ef42242ae2212d7a
Dhiraj155 <dhirajwani155@gmail.com>
2022-07-14 18:14:37 +0530

    Que_4.rs

---
+++ Basic_Types/Char_Bool_int/Bool_Que_4.rs
@@ -0,0 +1,17 @@
+
+// Make it work
+fn main() {
+    let f = true;
+    let t = true && false;
+    assert_eq!(t, f);
+
+    println!("Success!");
+}
+
+//Ans---
+
+fn main() {
+    let f = true;
+    let t = true || false;
+    assert_eq!(t, f);
+}
