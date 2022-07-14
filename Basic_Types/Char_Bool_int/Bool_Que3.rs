474135570e36c52800cfb72f078771afaa9a1ae2
Dhiraj155 <dhirajwani155@gmail.com>
2022-07-14 18:12:34 +0530

    Que 3

---
+++ Basic_Types/Char_Bool_int/Bool_Que3.rs
@@ -0,0 +1,21 @@
+
+// Make println! work
+fn main() {
+    let _f: bool = false;
+
+    let t = true;
+    if !t {
+        println!("Success!");
+    }
+} 
+
+//Ans---
+
+fn main() {
+    let _f: bool = false;
+
+    let t = false;
+    if !t {
+        println!("hello, world");
+    }
+} 
