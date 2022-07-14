a656faf2434dc8e74e2a3b964ba24707386b6c70
Dhiraj155 <dhirajwani155@gmail.com>
2022-07-14 20:53:19 +0530

    Que 2

---
+++ Basic_Types/Functions/Function_Que_2.rs
@@ -0,0 +1,19 @@
+fn main() {
+    print();
+ }
+ 
+ // Replace i32 with another type
+ fn print() -> i32 {
+    println!("Success!");
+ }
+
+ //Ans----
+
+ fn main() {
+    print();
+}
+
+// replace i32 with another type
+fn print() -> () {
+    println!("hello,world");
+}
