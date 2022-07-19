0e71e98a4b3b67d05dad3663b815b6fcdf2012de
Dhiraj155 <dhirajwani155@gmail.com>
2022-07-19 16:06:49 +0530

    Que5

---
+++ Compound_Types/Tuple/Tuple_Que5.rs
@@ -0,0 +1,27 @@
+// Destructure assignments.
+
+fn main() {
+    let (x, y, z);
+
+    // Fill the blank
+    __ = (1, 2, 3);
+    
+    assert_eq!(x, 3);
+    assert_eq!(y, 1);
+    assert_eq!(z, 2);
+
+    println!("Success!");
+}
+
+//ans---
+
+fn main() {
+    let (x, y, z);
+
+    // fill the blank
+    (y, z, x) = (1, 2, 3);
+    
+    assert_eq!(x, 3);
+    assert_eq!(y, 1);
+    assert_eq!(z, 2);
+}
