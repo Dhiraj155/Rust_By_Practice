6ae1f1e754abe9d4e73a23b7e2fe6ccb3797f911
Dhiraj155 <dhirajwani155@gmail.com>
2022-07-19 09:49:13 +0530

    Que7

---
+++ Ownership_and_Borrowing/Ownership/Ownership_mutability_Que7.rs
@@ -0,0 +1,24 @@
+
+fn main() {
+    let x = Box::new(5);
+    
+    let ...      // Implement this line, dont change other lines!
+    
+    *y = 4;
+    
+    assert_eq!(*x, 5);
+
+    println!("Success!");
+}
+
+//ans---
+
+fn main() {
+    let x = Box::new(5);
+    
+    let mut y = Box::new(3);       // implement this line, dont change other lines!
+    
+    *y = 4;
+    
+    assert_eq!(*x, 5);
+}
