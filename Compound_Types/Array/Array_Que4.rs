bd9dc94f297e102163e2618da0e0f436c58f0ab1
Dhiraj155 <dhirajwani155@gmail.com>
2022-07-19 13:23:10 +0530

    Que 4

---
+++ Compound_Types/Array/Array_Que4.rs
@@ -0,0 +1,16 @@
+//All elements in an array must be of the same type
+
+
+fn main() {
+    // Fix the error
+    let _arr = [1, 2, '3'];
+
+    println!("Success!");
+}
+
+//ans--
+
+fn main() {
+    // fix the error
+    let _arr = [1, 2, 3];
+}
