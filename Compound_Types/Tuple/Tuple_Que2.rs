aa40f6dd7c19523600dcfe0e0a4d7bb95d0230e1
Dhiraj155 <dhirajwani155@gmail.com>
2022-07-19 16:00:54 +0530

    Que 2

---
+++ Compound_Types/Tuple/Tuple_Que2.rs
@@ -0,0 +1,16 @@
+//Members can be extracted from the tuple using indexing.
+
+// Make it work
+fn main() {
+    let t = ("i", "am", "sunface");
+    assert_eq!(t.1, "sunface");
+
+    println!("Success!");
+}
+
+//ans---
+
+fn main() {
+    let t = ("i", "am", "sunface");
+    assert_eq!(t.2, "sunface");
+ }
