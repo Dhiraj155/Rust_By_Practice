7269ea1bf740fe4dab955880ba0bdff22480d8b9
Dhiraj155 <dhirajwani155@gmail.com>
2022-07-19 10:23:31 +0530

    Que9

---
+++ Ownership_and_Borrowing/Referance_and_Borrowing/mutability_Que_9.rs
@@ -0,0 +1,25 @@
+
+// This code has no errors!
+fn main() {
+    let mut s = String::from("hello, ");
+
+    borrow_object(&s);
+    
+    s.push_str("world");
+
+    println!("Success!");
+}
+
+fn borrow_object(s: &String) {}
+
+//ans---
+
+fn main() {
+    let mut s = String::from("hello, ");
+
+    borrow_object(&s);
+    
+    s.push_str("world");
+}
+
+fn borrow_object(s: &String) {}
