88c1c6910052ded1970d639ee417926aed533be5
Dhiraj155 <dhirajwani155@gmail.com>
2022-07-19 13:27:19 +0530

    Que 6

---
+++ Compound_Types/Array/Array_Que6.rs
@@ -0,0 +1,28 @@
+//Out of bounds indexing causes panic
+
+
+// Fix the error
+fn main() {
+    let names = [String::from("Sunfei"), "Sunface".to_string()];
+    
+    // `Get` returns an Option<T>, it's safe to use
+    let name0 = names.get(0).unwrap();
+
+    // But indexing is not safe
+    let _name1 = &names[2];
+
+    println!("Success!");
+}
+
+
+//ans---
+
+fn main() {
+    let names = [String::from("Sunfei"), "Sunface".to_string()];
+
+    // `get` returns an Option<T>, it's safe to use
+    let name0 = names.get(0).unwrap();
+
+    // but indexing is not safe
+    let _name1 = &names[1];
+}
