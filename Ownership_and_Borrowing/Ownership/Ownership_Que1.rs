6c50406edfc660e0c3a7948920f60c92df085681
Dhiraj155 <dhirajwani155@gmail.com>
2022-07-19 09:38:16 +0530

    Que1

--- Ownership_and_Borrowing/Ownership/Ownership_Que1.rs
+++ Ownership_and_Borrowing/Ownership/Ownership_Que1.rs
@@ -13,3 +13,21 @@ fn main() {
     let y = x.clone();
     println!("{},{}",x,y);
 }
+
+fn main() {
+    let x = "hello, world";
+    let y = x;
+    println!("{},{}",x,y);
+}
+
+fn main() {
+    let x = &String::from("hello, world");
+    let y = x;
+    println!("{},{}",x,y);
+}
+
+fn main() {
+    let x = 10;
+    let y = x;
+    println!("{},{}",x,y);
+}
