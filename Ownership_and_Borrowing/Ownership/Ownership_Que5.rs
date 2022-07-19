b6c8d2f4888d91e02bae23827b4b27634606952d
Dhiraj155 <dhirajwani155@gmail.com>
2022-07-19 09:45:39 +0530

    Que5

---
+++ Ownership_and_Borrowing/Ownership/Ownership_Que5.rs
@@ -0,0 +1,14 @@
+// Don't use clone ,use copy instead
+fn main() {
+    let x = (1, 2, (), "hello".to_string());
+    let y = x.clone();
+    println!("{:?}, {:?}", x, y);
+}
+
+//Ans---
+
+fn main() {
+    let x = (1, 2, (), "hello");
+    let y = x;
+    println!("{:?}, {:?}", x, y);
+}
