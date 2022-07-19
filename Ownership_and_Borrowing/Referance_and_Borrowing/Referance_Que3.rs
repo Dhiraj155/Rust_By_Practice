7a30cc1200db18e4b8e2112ea311448fc870ef3e
Dhiraj155 <dhirajwani155@gmail.com>
2022-07-19 10:06:20 +0530

    Que3

---
+++ Ownership_and_Borrowing/Referance_and_Borrowing/Referance_Que3.rs
@@ -0,0 +1,22 @@
+
+// Fix error
+fn main() {
+    let mut s = String::from("hello, ");
+
+    borrow_object(s);
+
+    println!("Success!");
+}
+
+fn borrow_object(s: &String) {}
+
+
+//ans---
+
+fn main() {
+    let mut s = String::from("hello, ");
+
+    borrow_object(&s)
+}
+
+fn borrow_object(s: &String) {}
