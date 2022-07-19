358ec5f46fa196fe5062588e40346af09ff225dc
Dhiraj155 <dhirajwani155@gmail.com>
2022-07-19 12:10:51 +0530

    Que1

---
+++ Compound_Types/String/str_and_&str_Que1.rs
@@ -0,0 +1,13 @@
+
+// Fix error without adding new line
+fn main() {
+    let s: str = "hello, world";
+
+    println!("Success!");
+}
+
+//ans---
+
+fn main() {
+    let s: &str = "hello, world";
+ }
