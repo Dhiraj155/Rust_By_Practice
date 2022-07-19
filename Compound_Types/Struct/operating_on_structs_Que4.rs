16c9ba9f3894b6344f703b3a6d35c6d0fba97600
Dhiraj155 <dhirajwani155@gmail.com>
2022-07-19 16:23:09 +0530

    Que 4

---
+++ Compound_Types/Struct/operating_on_structs_Que4.rs
@@ -0,0 +1,42 @@
+//You can make a whole struct mutable when instantiating it, but Rust doesn't allow us to mark only certain fields as mutable.
+
+
+// Fill the blank and fix the error without adding/removing new line
+struct Person {
+    name: String,
+    age: u8,
+}
+fn main() {
+    let age = 18;
+    let p = Person {
+        name: String::from("sunface"),
+        age,
+    };
+
+    // How can you believe sunface is only 18? 
+    p.age = 30;
+
+    // Fill the blank
+    __ = String::from("sunfei");
+
+    println!("Success!");
+}
+
+//ans----
+
+struct Person {
+    name: String,
+    age: u8,
+}
+fn main() {
+    let age = 18;
+    let mut p = Person {
+        name: String::from("sunface"),
+        age,
+    };
+
+    // how can you believe sunface is only 18? 
+    p.age = 30;
+
+    p.name = String::from("sunfei");
+}
