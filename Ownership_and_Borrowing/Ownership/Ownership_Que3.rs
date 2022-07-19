a0f896c27ca5ce9ab3bfeeb38cfe91fc551cd746
Dhiraj155 <dhirajwani155@gmail.com>
2022-07-19 09:42:13 +0530

    Que3

---
+++ Ownership_and_Borrowing/Ownership/Ownership_Que3.rs
@@ -0,0 +1,39 @@
+
+fn main() {
+    let s = give_ownership();
+    println!("{}", s);
+}
+
+// Only modify the code below!
+fn give_ownership() -> String {
+    let s = String::from("hello, world");
+    // Convert String to Vec
+    let _s = s.into_bytes();
+    s
+}
+
+//ans---
+
+fn main() {
+    let s = give_ownership();
+    println!("{}", s);
+}
+
+// Only modify the code below!
+fn give_ownership() -> String {
+    let s = String::from("hello, world");
+    // convert String to Vec
+    let _s = s.as_bytes();
+    s
+}
+
+fn main() {
+    let s = give_ownership();
+    println!("{}", s);
+}
+
+// Only modify the code below!
+fn give_ownership() -> String {
+    let s = String::from("hello, world");
+    s
+}
