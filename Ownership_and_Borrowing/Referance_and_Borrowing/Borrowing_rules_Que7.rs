a1a35c5d6a4f441c2ee3e48394b17940e0adb72c
Dhiraj155 <dhirajwani155@gmail.com>
2022-07-19 10:17:41 +0530

    Que7

---
+++ Ownership_and_Borrowing/Referance_and_Borrowing/Borrowing_rules_Que7.rs
@@ -0,0 +1,24 @@
+
+// Remove something to make it work
+// Don't remove a whole line !
+fn main() {
+    let mut s = String::from("hello");
+
+    let r1 = &mut s;
+    let r2 = &mut s;
+
+    println!("{}, {}", r1, r2);
+
+    println!("Success!");
+}
+
+//ans---
+
+fn main() {
+    let s = String::from("hello");
+
+    let r1 = &s;
+    let r2 = &s;
+
+    println!("{}, {}", r1, r2);
+}
