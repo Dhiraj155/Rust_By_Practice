d67fa71951c3dcf47484b41c02bd769228d1ba5c
Dhiraj155 <dhirajwani155@gmail.com>
2022-07-19 10:12:36 +0530

    Que6

---
+++ Ownership_and_Borrowing/Referance_and_Borrowing/ref_Que6.rs
@@ -0,0 +1,41 @@
+
+//ref can be used to take references to a value, similar to &
+fn main() {
+    let c = '中';
+
+    let r1 = &c;
+    // Fill the blank，dont change other code
+    let __ r2 = c;
+
+    assert_eq!(*r1, *r2);
+    
+    // Check the equality of the two address strings
+    assert_eq!(get_addr(r1),get_addr(r2));
+
+    println!("Success!");
+}
+
+// Get memory address string
+fn get_addr(r: &char) -> String {
+    format!("{:p}", r)
+}
+
+//ans---
+
+fn main() {
+    let c = '中';
+
+    let r1 = &c;
+    // fill the blank，dont change other code
+    let ref r2 = c;
+
+    assert_eq!(*r1, *r2);
+    
+    // check the equality of the two address strings
+    assert_eq!(get_addr(r1),get_addr(r2));
+}
+
+// get memory address string
+fn get_addr(r: &char) -> String {
+    format!("{:p}", r)
+}
