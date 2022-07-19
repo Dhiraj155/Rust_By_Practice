c636b5241910543fa8845873a3dc4e28c050e447
Dhiraj155 <dhirajwani155@gmail.com>
2022-07-19 12:14:36 +0530

    Que2

--- Compound_Types/String/str_and_&str_Que1.rs
+++ Compound_Types/String/str_and_&str_Que1.rs
@@ -1,4 +1,4 @@
-
+// We can't use str type in normal ways, but we can use &str
 // Fix error without adding new line
 fn main() {
     let s: str = "hello, world";
---
+++ Compound_Types/String/str_and_&str_Que2.rs
@@ -0,0 +1,31 @@
+// We can only use str by boxed it, & can be used to convert Box<str> to &str
+
+// Fix the error with at least two solutions
+fn main() {
+    let s: Box<str> = "hello, world".into();
+    greetings(s)
+}
+
+fn greetings(s: &str) {
+    println!("{}",s)
+}
+
+//ans---
+
+fn main() {
+    let s: Box<str> = "hello, world".into();
+    greetings(&s)
+ }
+ 
+ fn greetings(s: &str) {
+     println!("{}",s)
+ }
+
+ fn main() {
+    let s: Box<&str> = "hello, world".into();
+    greetings(*s)
+}
+
+fn greetings(s: &str) {
+    println!("{}", s);
+}
