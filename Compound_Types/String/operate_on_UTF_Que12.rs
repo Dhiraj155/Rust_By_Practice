8f5fe2330ebc04516ba7c5d45766ecd6e16206ea
Dhiraj155 <dhirajwani155@gmail.com>
2022-07-19 13:12:16 +0530

    Que12

---
+++ Compound_Types/String/operate_on_UTF_Que12.rs
@@ -0,0 +1,15 @@
+
+fn main() {
+    // Fill the blank to print each char in "你好，世界"
+    for c in "你好，世界".__ {
+        println!("{}", c)
+    }
+}
+
+//ans---
+
+fn main() {
+    for c in "你好，世界".chars() {
+        println!("{}", c)
+    }
+}
