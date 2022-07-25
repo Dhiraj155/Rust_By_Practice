49fc8492af354845812361a4de60c282f9232dd4
Dhiraj155 <dhirajwani155@gmail.com>
2022-07-25 21:08:57 +0530

    Que 7

---
+++ Flow_control/continue_and_break_7.rs
@@ -0,0 +1,31 @@
+//Use break to break the loop.
+
+
+// Fill in the blank
+fn main() {
+    let mut n = 0;
+    for i in 0..=100 {
+       if n == 66 {
+           __
+       }
+       n += 1;
+    }
+
+    assert_eq!(n, 66);
+
+    println!("Success!");
+}
+
+//ans---
+
+fn main() {
+    let mut n = 0;
+    for i in 0..=100 {
+        if n == 66 {
+            break;
+        }
+        n += 1;
+    }
+
+    assert_eq!(n, 66);
+}
