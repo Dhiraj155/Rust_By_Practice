0d24fc55898b82bd402069c0d99d84cb87d6e52f
Dhiraj155 <dhirajwani155@gmail.com>
2022-07-29 20:01:08 +0530

    Que 4

---
+++ Pattern_Match/match_matches_if_let/matches_Que4.rs
@@ -0,0 +1,24 @@
+//matches! looks like match, but can do something different.
+
+
+fn main() {
+    let alphabets = ['a', 'E', 'Z', '0', 'x', '9' , 'Y'];
+
+    // Fill the blank with `matches!` to make the code work
+    for ab in alphabets {
+        assert!(__)
+    }
+
+    println!("Success!");
+} 
+
+//ans--
+
+fn main() {
+    let alphabets = ['a', 'E', 'Z', '0', 'x', '9' , 'Y'];
+
+    // fill the blank with `matches!` to make the code work
+    for ab in alphabets {
+        assert!(matches!(ab, 'a'..='z' | 'A'..='Z' | '0'..='9'))
+    }
+} 
