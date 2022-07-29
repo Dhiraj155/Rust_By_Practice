05ad5df550d522eff2879dd30479d1fd6952ec63
Dhiraj155 <dhirajwani155@gmail.com>
2022-07-29 19:57:22 +0530

    Que 2

---
+++ Pattern_Match/match_matches_if_let/match_Que2.rs
@@ -0,0 +1,33 @@
+//Match is an expression, so we can use it in assignments
+
+
+fn main() {
+    let boolean = true;
+
+    // Fill the blank with a match expression:
+    //
+    // boolean = true => binary = 1
+    // boolean = false =>  binary = 0
+    let binary = __;
+
+    assert_eq!(binary, 1);
+
+    println!("Success!");
+}
+
+//ans---
+
+fn main() {
+    let boolean = true;
+
+    // fill the blank with an match expression:
+    //
+    // boolean = true => binary = 1
+    // boolean = false =>  binary = 0
+    let binary = match boolean {
+        true => 1,
+        false => 0
+    };
+
+    assert_eq!(binary, 1);
+}
