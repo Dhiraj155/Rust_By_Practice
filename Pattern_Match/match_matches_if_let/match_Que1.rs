e32a55321b43ff2d10a16ef70b12a0070589ab91
Dhiraj155 <dhirajwani155@gmail.com>
2022-07-29 19:55:09 +0530

    Que 1

---
+++ Pattern_Match/match_matches_if_let/match_Que1.rs
@@ -0,0 +1,39 @@
+
+// Fill the blanks
+enum Direction {
+    East,
+    West,
+    North,
+    South,
+}
+
+fn main() {
+    let dire = Direction::South;
+    match dire {
+        Direction::East => println!("East"),
+        __  => { // Matching South or North here
+            println!("South or North");
+        },
+        _ => println!(__),
+    };
+}
+
+//ans---
+
+enum Direction {
+    East,
+    West,
+    North,
+    South,
+}
+
+fn main() {
+    let dire = Direction::South;
+    match dire {
+        Direction::East => println!("East"),
+        Direction::South | Direction::North  => { // matching South or North here
+            println!("South or North");
+        },
+        _ => println!("West"),
+    };
+}
