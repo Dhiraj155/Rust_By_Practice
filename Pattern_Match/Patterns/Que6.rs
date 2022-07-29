547131adf77ee98f88e18fb965588eb49660d6b8
Dhiraj155 <dhirajwani155@gmail.com>
2022-07-29 20:19:22 +0530

    Que 6

---
+++ Pattern_Match/Patterns/Que6.rs
@@ -0,0 +1,25 @@
+// Using pattern &mut V to match a mutable reference needs you to be very careful, due to V being a value after matching.
+
+
+// FIX the error with least changing
+// DON'T remove any code line
+fn main() {
+    let mut v = String::from("hello,");
+    let r = &mut v;
+
+    match r {
+       &mut value => value.push_str(" world!") 
+    }
+}
+
+//ans--
+
+fn main() {
+    let mut v = String::from("hello,");
+    let r = &mut v;
+
+    match r {
+        // The type of value is &mut String
+       value => value.push_str(" world!") 
+    }
+}
