1822a5f3dca254dcd22bb413b7071d3b1e54e0f5
Dhiraj155 <dhirajwani155@gmail.com>
2022-07-30 21:13:19 +0530

    Que 3

---
+++ Generics And Traits/Generics/Struct_and_impl_Que3.rs
@@ -0,0 +1,22 @@
+
+// Implement struct Point to make it work.
+
+
+fn main() {
+    let integer = Point { x: 5, y: 10 };
+    let float = Point { x: 1.0, y: 4.0 };
+
+    println!("Success!");
+}
+
+//ans---
+
+struct Point<T> {
+    x: T,
+    y: T,
+}
+
+fn main() {
+    let integer = Point { x: 5, y: 10 };
+    let float = Point { x: 1.0, y: 4.0 };
+}
