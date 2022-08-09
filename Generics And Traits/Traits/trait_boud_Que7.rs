ef8c0405dd3f2e12a58e220808040ae2246125b0
Dhiraj155 <dhirajwani155@gmail.com>
2022-08-09 16:13:33 +0530

    Que 7

---
+++ Generics And Traits/Traits/trait_boud_Que7.rs
@@ -0,0 +1,23 @@
+//The impl Trait syntax works for straightforward cases but is actually syntax sugar for a longer form, which is called a trait bound.
+
+//When working with generics, the type parameters often must use traits as bounds to stipulate what functionality a type implements.
+
+fn main() {
+    assert_eq!(sum(1, 2), 3);
+}
+
+// Implement `fn sum` with trait bound in two ways.
+fn sum<T>(x: T, y: T) -> T {
+    x + y
+}
+
+//ans--
+
+fn main() {
+    assert_eq!(sum(1, 2), 3);
+    assert_eq!(sum(1.0, 2.0), 3.0);
+}
+
+fn sum<T: std::ops::Add<Output = T>>(x: T, y: T) -> T {
+    x + y
+}
